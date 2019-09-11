3f65bf9447fbe134199ef22c6fe7c7926f7d901e
yeunah <kjbnam@naver.com>
2019-09-11 15:39:47 +0900

    커밋

---
+++ chatting/chatting/mainform.cpp
@@ -0,0 +1,12 @@
+#include "mainform.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+void main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	chatting::mainform form;
+	Application::Run(% form); 
+}
