0cdde0d840b695e9ec0b6c64e2fac3873b743d21
yeunah <kjbnam@naver.com>
2019-09-11 15:44:41 +0900

    20190911_5

--- chatting/chatting/mainform.h
+++ chatting/chatting/mainform.h
@@ -44,7 +44,7 @@ namespace chatting {
 #pragma region Windows Form Designer generated code
 		/// <summary>
 		/// 디자이너 지원에 필요한 메서드입니다. 
-		/// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+		/// 이 메서드의 내용을 코드 편집기로 수정하지 마세요. 
 		/// </summary>
 		void InitializeComponent(void)
 		{
